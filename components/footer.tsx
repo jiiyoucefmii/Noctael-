070efeec8ce415bb4a22bdcf62e9dcac60c5e63c
jiiyoucefmii <jimitakos24@gmail.com>
2025-07-27 18:48:11 +0100

    added facebook

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link"
-import { Instagram } from "lucide-react"
+import { Instagram, Facebook } from "lucide-react"
 
 export default function Footer() {
   return (
@@ -10,6 +10,10 @@ export default function Footer() {
             <h3 className="mb-2 text-base font-bold">NOCTAEL</h3> {/* Reduced margin and font size */}
             <p className="text-xs text-gray-400 max-w-md mx-auto">Premium clothing brand for those who embrace the darkness.</p> {/* Reduced text size */}
             <div className="mt-2 flex space-x-4 justify-center"> {/* Reduced margin-top */}
+              <Link href="https://www.facebook.com/share/19AfhhX9kS/?mibextid=wwXIfr" className="text-gray-400 hover:text-white" target="_blank" rel="noopener noreferrer">
+                <Facebook className="h-4 w-4" /> {/* Reduced icon size */}
+                <span className="sr-only">Facebook</span>
+              </Link>
               <Link href="https://www.instagram.com/noctael.co" className="text-gray-400 hover:text-white" target="_blank" rel="noopener noreferrer">
                 <Instagram className="h-4 w-4" /> {/* Reduced icon size */}
                 <span className="sr-only">Instagram</span>
